e4b2f2f5f68130dfb1b132d75b2e894a4bc54c3d
_[Burn]_ <baryan.acb@gmail.com>
2021-04-14 09:18:49 -0500

    version 2

--- js/main.js
+++ js/main.js
@@ -1,158 +1,129 @@
-// helper functions
-const PI2 = Math.PI * 2
-const random = (min, max) => Math.random() * (max - min + 1) + min | 0
-const timestamp = _ => new Date().getTime()
-
-// container
-class Birthday {
-  constructor() {
-    this.resize()
-
-    // create a lovely place to store the firework
-    this.fireworks = []
-    this.counter = 0
-
-  }
-  
-  resize() {
-    this.width = canvas.width = window.innerWidth
-    let center = this.width / 2 | 0
-    this.spawnA = center - center / 4 | 0
-    this.spawnB = center + center / 4 | 0
+var w = c.width = window.innerWidth,
+    h = c.height = window.innerHeight,
+    ctx = c.getContext( '2d' ),
     
-    this.height = canvas.height = window.innerHeight
-    this.spawnC = this.height * .1
-    this.spawnD = this.height * .5
-    
-  }
-  
-  onClick(evt) {
-     let x = evt.clientX || evt.touches && evt.touches[0].pageX
-     let y = evt.clientY || evt.touches && evt.touches[0].pageY
-     
-     let count = random(3,5)
-     for(let i = 0; i < count; i++) this.fireworks.push(new Firework(
-        random(this.spawnA, this.spawnB),
-        this.height,
-        x,
-        y,
-        random(0, 260),
-        random(30, 110)))
-          
-     this.counter = -1
-     
-  }
-  
-  update(delta) {
-    ctx.globalCompositeOperation = 'hard-light'
-    ctx.fillStyle = `rgba(20,20,20,${ 7 * delta })`
-    ctx.fillRect(0, 0, this.width, this.height)
+    fireworks = [],
+    tick = 0;
 
-    ctx.globalCompositeOperation = 'lighter'
-    for (let firework of this.fireworks) firework.update(delta)
 
-    // if enough time passed... create new new firework
-    this.counter += delta * 3 // each second
-    if (this.counter >= 1) {
-      this.fireworks.push(new Firework(
-        random(this.spawnA, this.spawnB),
-        this.height,
-        random(0, this.width),
-        random(this.spawnC, this.spawnD),
-        random(0, 360),
-        random(30, 110)))
-      this.counter = 0
+ctx.fillStyle = '#111';
+ctx.fillRect( 0, 0, w, h );
+
+ctx.font = '60px Verdana';
+var text = '',
+    len = ctx.measureText( text ).width;
+
+function anim(){
+
+  window.requestAnimationFrame( anim );
+  
+  ++tick;
+  
+  if( fireworks.length < 50 && Math.random() < .1 )
+    fireworks.push( new Firework );
+  
+  ctx.fillStyle = 'rgba( 0, 0, 0, .04 )';
+  ctx.fillRect( 0, 0, w, h );
+  
+  ctx.fillStyle = 'hsl(0,0%,light%)'.replace( 'light', 50 + 20 * Math.sin( tick / 10 ) );
+  ctx.fillText( text, w / 2 - len / 2, h / 2 - 10 );
+  
+  fireworks.map( function( firework ) { firework.step(); } );
+}
+function Firework(){
+
+  this.reset();
+}
+Firework.prototype.reset = function(){
+  
+  this.dx = Math.random() * 300 * ( Math.random() < .5 ? 1 : -1 );
+
+  this.sx = Math.random() * w;
+  this.ex = this.sx + this.dx;
+  this.ey = Math.random() * h;
+  
+  this.px = this.sx;
+  this.py = 0;
+  
+  this.time = ( 30 + 30 * Math.random() ) |0;
+  this.tick = 0;
+  
+  this.size = ( 2 + 2 * Math.random() ) |0;
+  this.shatters = ( 5 + 5 * Math.random() ) |0;
+  this.radiant = Math.PI * 2 / this.shatters;
+  
+  this.shatterContainer = [];
+  this.dead = false;
+}
+Firework.prototype.step = function(){
+  
+  if( !this.dead ){
+    ++this.tick;
+    var prop = this.tick / this.time,
+        nx = this.sx + this.dx * ( 1 + Math.sin( -Math.PI / 2 + prop * Math.PI / 2 ) ),
+        ny = prop * this.ey;
+
+    ctx.strokeStyle = 'hsl(hue,80%,50%)'.replace( 'hue', tick / 3 + nx / w * 100 );
+    ctx.lineWidth = this.size;
+    ctx.beginPath();
+    ctx.moveTo( this.px, h - this.py );
+    ctx.lineTo( nx, h - ny );
+    ctx.stroke();
+
+    this.px = nx;
+    this.py = ny;
+
+    if( prop >= 1 ){
+      
+      this.dead = true;
+
+      for( var i = 0; i < this.shatters; ++i )
+        this.shatterContainer.push( new Shatter( nx, ny, this.radiant * i ) );
     }
 
-    // remove the dead fireworks
-    if (this.fireworks.length > 1000) this.fireworks = this.fireworks.filter(firework => !firework.dead)
-
-  }
-}
-
-class Firework {
-  constructor(x, y, targetX, targetY, shade, offsprings) {
-    this.dead = false
-    this.offsprings = offsprings
-
-    this.x = x
-    this.y = y
-    this.targetX = targetX
-    this.targetY = targetY
-
-    this.shade = shade
-    this.history = []
-  }
-  update(delta) {
-    if (this.dead) return
-
-    let xDiff = this.targetX - this.x
-    let yDiff = this.targetY - this.y
-    if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) { // is still moving
-      this.x += xDiff * 2 * delta
-      this.y += yDiff * 2 * delta
-
-      this.history.push({
-        x: this.x,
-        y: this.y
-      })
-
-      if (this.history.length > 20) this.history.shift()
-
-    } else {
-      if (this.offsprings && !this.madeChilds) {
-        
-        let babies = this.offsprings / 2
-        for (let i = 0; i < babies; i++) {
-          let targetX = this.x + this.offsprings * Math.cos(PI2 * i / babies) | 0
-          let targetY = this.y + this.offsprings * Math.sin(PI2 * i / babies) | 0
-
-          birthday.fireworks.push(new Firework(this.x, this.y, targetX, targetY, this.shade, 0))
-
-        }
-
+    if( this.x < 0 || this.x > w )
+      this.reset();
+    
+  } else {
+  
+    for( var i = 0; i < this.shatterContainer.length; ++i ) {
+    
+      var shatter = this.shatterContainer[ i ];
+      
+      shatter.step();
+      
+      if( shatter.x < 0 || shatter.y > w || shatter.y > h ){
+      
+        this.shatterContainer.splice( i, 1 );
+        --i;
       }
-      this.madeChilds = true
-      this.history.shift()
     }
     
-    if (this.history.length === 0) this.dead = true
-    else if (this.offsprings) { 
-        for (let i = 0; this.history.length > i; i++) {
-          let point = this.history[i]
-          ctx.beginPath()
-          ctx.fillStyle = 'hsl(' + this.shade + ',100%,' + i + '%)'
-          ctx.arc(point.x, point.y, 1, 0, PI2, false)
-          ctx.fill()
-        } 
-      } else {
-      ctx.beginPath()
-      ctx.fillStyle = 'hsl(' + this.shade + ',100%,50%)'
-      ctx.arc(this.x, this.y, 1, 0, PI2, false)
-      ctx.fill()
-    }
-
+    if( this.shatterContainer.length === 0 )
+      this.reset();
   }
 }
+function Shatter( x, y, rad ){
 
-let canvas = document.getElementById('birthday');
-let ctx = canvas.getContext('2d');
+  this.x = x;
+  this.y = h - y;
+  this.vx = ( Math.random() + 1 ) * Math.cos( rad );
+  this.vy = ( Math.random() + 1 ) * Math.sin( rad );
+}
+Shatter.prototype.step = function(){
 
-let then = timestamp()
+  var nx = this.x + ( this.vx *= .999 ),
+      ny = this.y + ( this.vy += .02 );
+  
+  ctx.strokeStyle = 'hsl(hue,80%,50%)'.replace( 'hue', tick / 3 + nx / w * 100 );
+  ctx.lineWidth = .5 + Math.random();
+  ctx.beginPath();
+  ctx.moveTo( this.x, this.y );
+  ctx.lineTo( nx, ny );
+  ctx.stroke();
+  
+  this.x = nx;
+  this.y = ny;
+}
 
-let birthday = new Birthday
-window.onresize = () => birthday.resize()
-document.onclick = evt => birthday.onClick(evt)
-document.ontouchstart = evt => birthday.onClick(evt)
-
-  ;(function loop(){
-  	requestAnimationFrame(loop)
-
-  	let now = timestamp()
-  	let delta = now - then
-
-    then = now
-    birthday.update(delta / 1000)
-  	
-
-  })()
+anim();
